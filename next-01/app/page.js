57cd598d8f7931154f9ac663cf7292f97dcdf293
Beatriz Farias Silva <beatrizfariasilva1@gmail.com>
2026-02-12 01:21:29 +0000

    alteração 4

--- next-01/app/page.js
+++ next-01/app/page.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <div>
-        <p>Olá, amigo.</p>
+        <p style={{fontSize: "40px"}}> Olá, amigo.</p>
         <p> Gostaria de compatilhar que no início deste terceiro período, estou sendo introduzida a disciplina de Front-End. Para isso, aprenderemos como utilizar uma nova biblioteca dentro do JS, o React, e utilizaremos Next.js como framework. Está sendo bem legal aprender algo completamente novo.</p>
       </div>
     </div>
