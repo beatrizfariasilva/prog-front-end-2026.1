463321de50fe5da8e932d5af07ef4b28ddafdd6a
Beatriz Farias Silva <beatrizfariasilva1@gmail.com>
2026-02-12 01:18:27 +0000

    alteração 4

--- next-01/app/page.js
+++ next-01/app/page.js
@@ -3,7 +3,10 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <div>{"Olá, amigo.\nGostaria de compatilhar que no início deste terceiro período, estou sendo introduzida a disciplina de Front-End. Para isso, aprenderemos como utilizar uma nova biblioteca dentro do JS, o React, e utilizaremos Next.js como framework. Está sendo bem legal aprender algo completamente novo."}</div>
+      <div>
+        <p>Olá, amigo.</p>
+        <p> Gostaria de compatilhar que no início deste terceiro período, estou sendo introduzida a disciplina de Front-End. Para isso, aprenderemos como utilizar uma nova biblioteca dentro do JS, o React, e utilizaremos Next.js como framework. Está sendo bem legal aprender algo completamente novo.</p>
+      </div>
     </div>
   );
 }
