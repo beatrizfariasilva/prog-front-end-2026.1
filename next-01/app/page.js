964d37e84bd2b874c82ce861f484de2e7accbb7f
Beatriz Farias Silva <beatrizfariasilva1@gmail.com>
2026-02-20 00:30:52 +0000

    alteração 6

--- next-01/app/page.js
+++ next-01/app/page.js
@@ -1,11 +1,13 @@
 import Image from "next/image";
+import Profile from "@/components/profile";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+    <div className="flex flex-col min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <div>
-        <p style={{fontSize: "80px"}}> Olá, amigo.</p>
-        <p> Gostaria de compatilhar que venho sido introduzida à disciplina de Front-End. Vem sido bastante interessante aprender a como manipular uma nova biblioteca dentro do JS - o React - e usar Next.js como framework. Está sendo bem legal aprender algo completamente novo e diferente dos períodos anteriores.</p>
+        Olá! Essa é a minha primeira conexão entre pastas em React
+        <Profile />
+        <Profile />
       </div>
     </div>
   );
