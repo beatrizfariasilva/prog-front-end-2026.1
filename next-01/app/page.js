10eb5bf7c4b6088d6d09db62a076f8cc94b5cad4
Beatriz Farias Silva <beatrizfariasilva1@gmail.com>
2026-03-09 21:14:19 +0000

    atividade 09/03/2026

--- next-01/app/page.js
+++ next-01/app/page.js
@@ -3,6 +3,12 @@ import Profile from "@/components/profile";
 export default function Home() {
   return (
     <div className="flex flex-col min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <h1 style={{
+        textAlign: "center",
+        marginBottom: "90px",
+        marginTop: "-170px",
+        fontStyle: "italic"
+      }}> Site criado para a disciplina Programação Front-End </h1> 
       <div>
         <Profile />
       </div>
